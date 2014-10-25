157d01dd6cfdf2db21666906d97f0034aef9a9f5
Łukasz Gruner <lukasz@gruner.lu>
2014-10-26 00:04:01 +0200

    Initial implementation of aria2c api.

---
+++ aria2-mode.el
@@ -0,0 +1,405 @@
+;;; aria2-mode.el --- Controll aria2c commandline tool from Emacs
+
+;; Copyright (c) 2014 Łukasz Gruner
+
+;; Author: Łukasz Gruner <lukasz@gruner.lu>
+;; Maintainer: Łukasz Gruner <lukasz@gruner.lu>
+;; Version: 1
+;; Package-Requires: ((json "8") (url))
+;; URL: https://bitbucket.org/ukaszg/aria2-mode
+;; Created: 19/10/2014
+;; Keywords: download bittorrent aria2
+
+;; This file is not part of Emacs.
+
+
+;; This work ‘as-is’ we provide.
+;; No warranty express or implied.
+;; We’ve done our best,
+;; to debug and test.
+;; Liability for damages denied.
+;;
+;; Permission is granted hereby,
+;; to copy, share, and modify.
+;; Use as is fit,
+;; free or for profit.
+;; These rights, on this notice, rely.
+
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'eieio-base)
+(require 'json)
+(require 'url)
+(require 'subr-x)
+(require 'tabulated-list)
+
+(declare-function string-trim "subr-x" (string))
+(declare-function string-join "subr-x" (string))
+
+;;; Customization variables start here.
+
+(defgroup aria2 nil
+  "An interface for aria2c command."
+  :group 'external
+  :group 'execute
+  :prefix "aria2-")
+
+(defcustom aria2-list-buffer-name "*aria2: downloads list*"
+  "Name of buffer to use when showing downloads list."
+  :type '(string :tag "Buffer name")
+  :group 'aria2)
+
+(defcustom aria2-details-buffer-name "*aria2: details %s*"
+  "Name of buffer to use when showing file details,
+\"%s\" will be replaced by that file name."
+  :type '(string :tag "Buffer name")
+  :group 'aria2)
+
+(defcustom aria2-executable (executable-find "aria2c")
+  "Full path to aria2c binary."
+  :type 'file
+  :group 'aria2)
+
+(defcustom aria2-session-file (expand-file-name "aria2c.session" user-emacs-directory)
+  "Name of session file. Will be used with \"--save-session\" and \"--input-file\" options."
+  :type 'file
+  :group 'aria2)
+
+(defcustom aria2-download-directory (or (getenv "XDG_DOWNLOAD_DIR") (expand-file-name "~/"))
+  "Default directory to store downloaded files."
+  :type 'directory
+  :group 'aria2)
+
+(defcustom aria2-rcp-listen-port 6800
+  "Port on which JSONRCP server will listen."
+  :type '(integer :tag "Http port")
+  :group 'aria2
+  :set #'(lambda (sym value)
+           (set-default sym value)
+           (when (and (boundp 'aria2--cc)
+                      (not (null aria2--cc)))
+             ;; restart process on a new port
+             (let ((running (is-process-running aria2--cc)))
+               (when running (shutdown aria2--cc t))
+               (oset aria2--cc rcp-url (aria2--url))
+               (when running (run-process aria2--cc))))))
+
+(defcustom aria2-custom-args nil
+  "Additional arguments for aria2c. This should be a list of strings. See aria2c manual for supported options."
+  :type '(repeat (string :tag "Commandline argument."))
+  :group 'aria2)
+
+(defvar aria2--debug t
+  "Should json commands and replies be printed. Also enables url-debug from url package.")
+
+(defconst aria2--cc-file
+  (expand-file-name "aria2-controller.eieio" user-emacs-directory)
+  "File used to persist controller status between Emacs restarts.")
+
+;;; Utils start here.
+
+(defsubst aria2--url ()
+  "Returns encoded url, wiith proper port value."
+  (url-encode-url (format "http://localhost:%d/jsonrpc" aria2-rcp-listen-port)))
+
+(defsubst aria2--base64-encode-file (path)
+  "Returns base64-encoded string with contents of file on PATH."
+  (unless (file-exists-p path)
+    (signal 'aria2-err-file-doesnt-exist '(path)))
+  (with-current-buffer (find-file-noselect path)
+    (unwind-protect
+        (base64-encode-string (buffer-string) t)
+      (kill-buffer))))
+
+;;; Error definitions start here
+
+(define-error 'aria2-err-too-many-magnet-urls  "Only one magnet link per download is allowed" 'user-error)
+(define-error 'aria2-err-file-doesnt-exist     "File %s doesn't exist"                        'user-error)
+(define-error 'aria2-err-not-a-torrent-file    "This is not a .torrent file"                  'user-error)
+(define-error 'aria2-err-not-a-metalink-file   "This is not a .metalink file"                 'user-error)
+(define-error 'aria2-err-failed-to-start       "Failed to start %s %s"                        'error)
+(define-error 'aria2-err-no-executable         "Couldn't find `aria2c' executable, aborting"  'error)
+(define-error 'aria2-err-no-such-position-type "Wrong position type \"%s\""                   'error)
+
+;;; Aria2c process controller starts here.
+
+(defclass aria2-controller (eieio-persistent)
+  ((request-id :initform 0
+               :type integer
+               :docstring "Value of id field in JSONRPC data, gets incremented for each request.")
+   (rcp-url :initarg :url
+            :initform (aria2--url)
+            :type string
+            :docstring "Url on which aria2c listens for JSON RPC requests.")
+   (secret :initform
+           (or (let ((uuidgen (executable-find "uuidgen")))
+                 (and uuidgen
+                      (string-trim (shell-command-to-string uuidgen))))
+               (sha1 (format
+                      "%s%s%s%s%s%s%s%s%s"
+                      (user-uid) (emacs-pid) (system-name) (user-full-name) (current-time)
+                      (emacs-uptime) (buffer-string) (random) (recent-keys))))
+           :type string
+           :docstring "Secret value used for authentication with the aria2c process, for use with --rpc-secret= switch.")
+   (pid :initform -1
+        :type integer
+        :docstring "PID of the aria2c process, or -1 if process isn't running."))
+  :docstring "This takes care of starting/stopping aria2c process and provides methods for each remote command.")
+
+;;; Internal methods start here.
+
+(defmethod get-next-id ((this aria2-controller))
+  "Return next request id json form. Resets back to 1 upon reaching `most-positive-fixnum'"
+  (let ((id (1+ (oref this request-id))))
+    (oset this request-id (if (equal id most-positive-fixnum) 0 id))
+    id))
+
+(defmethod is-process-running ((this aria2-controller))
+  "Returns status of aria2c process."
+  (when (< 0 (oref this pid)) t))
+
+(defmethod run-process ((this aria2-controller))
+  "Starts aria2c process, if not already running."
+  (unless (is-process-running this)
+    (let ((options (delq nil
+                         (append
+                          aria2-custom-args
+                          `("-D" ;; Start in daemon mode (won't be managed by Emacs).
+                            "-q" ;; Be quiet.
+                            "--enable-rpc=true"
+                            ,(format "--rpc-secret=%s" (oref this secret))
+                            ,(format "--rpc-listen-port=%s" aria2-rcp-listen-port)
+                            ,(format "--dir=%s" aria2-download-directory)
+                            ,(format "--save-session=%s" aria2-session-file)
+                            ,(when (file-exists-p aria2-session-file)
+                               (format "--input-file=%s" aria2-session-file)))))))
+      (when aria2--debug (message "Starting process: %s\n\t%s" aria2-executable (string-join options " ")))
+      (oset this pid (process-id
+                      (apply 'start-process "aria2c" nil aria2-executable options)))
+      (unless (is-process-running this)
+        (signal 'aria2-err-failed-to-start '(aria2-executable (string-join options " ")))))))
+
+(defmethod make-request ((this aria2-controller) method &rest params)
+  "Calls a remote METHOD with PARAMS. Returns response alist."
+  (run-process this)
+  (let ((url-debug aria2--debug)
+        (url-request-method "POST")
+        (url-request-data (json-encode-alist
+                           (list
+                            (cons "jsonrpc" 2.0)
+                            (cons "id" (get-next-id this))
+                            (cons "method"  method)
+                            (cons "params" (vconcat
+                                            `(,(format "token:%s" (oref this secret)))
+                                            (delq nil params))))))
+        (url-request-extra-headers '(("Content-Type" . "application/json")))
+        url-history-track
+        json-response)
+    (when aria2--debug (message "SEND: %s" url-request-data))
+    (with-current-buffer (url-retrieve-synchronously (oref this rcp-url))
+      ;; read last line, where json response is
+      (goto-char (point-max))
+      (beginning-of-line)
+      (setq json-response (json-read))
+      (kill-buffer))
+    (when aria2--debug (message "RECV: %s" json-response))
+    json-response))
+
+;;; Api implementation starts here
+
+(defmethod addUri ((this aria2-controller) &rest urls)
+  "Add a list of http/ftp/bittorrent URLS, pointing at the same file.
+When sending magnet link, URLS must have only one element."
+  (let ((magnet-count (length
+                       (remove-if-not (lambda (u) (string-match-p "^magnet://" u)) urls))))
+    (when (or (< 1 magnet-count) (and (< 0 magnet-count) (< 1 (length urls))))
+      (signal 'aria2-err-too-many-magnet-urls nil)))
+  (make-request this "aria2.addUri" (vconcat urls)))
+
+(defmethod addTorrent ((this aria2-controller) path)
+  "Add PATH pointing at a torrent file to download list."
+  (unless (file-exists-p path)
+    (signal 'aria2-err-file-doesnt-exist '(path)))
+  (unless (string-match-p "\\.torrent$" path)
+    (signal 'aria2-err-not-a-torrent-file nil))
+  (make-request this "aria2.addTorrent" (aria2--base64-encode-file path)))
+
+(defmethod addMetalink ((this aria2-controller) path)
+  "Add local .metalink PATH to download list."
+  (unless (file-exists-p path)
+    (signal 'aria2-err-file-doesnt-exist '(path)))
+  (unless (string-match-p "\\.meta\\(?:4\\|link\\)$" path)
+    (signal 'aria2-err-not-a-metalink-file nil))
+  (make-request this "aria2.addMetalink" (aria2--base64-encode-file path)))
+
+(defmethod remove-download ((this aria2-controller) gid &optional force)
+  "Remove download identified by GID. If FORCE don't unregister download at bittorrent tracker."
+  (make-request this (if force "aria2.forceRemove" "aria2.remove") gid))
+
+(defmethod pause ((this aria2-controller) gid &optional force)
+  "Pause download identified by GID. If FORCE don't unregister download at bittorrent tracker."
+  (make-request (if force "aria2.forcePause" "aria2.pause") gid))
+
+(defmethod pauseAll ((this aria2-controller) &optional force)
+  "Pause all downloads. If FORCE don't unregister download at bittorrent tracker."
+  (make-request (if force "aria2.forcePauseAll" "aria2.pauseAll") gid))
+
+(defmethod unpause ((this aria2-controller) gid)
+  "Unpause download identified by GID."
+  (make-request "aria2.unpause" gid))
+
+(defmethod unpauseAll ((this aria2-controller))
+  "Unpause all downloads."
+  (make-request "aria2.unpauseAll"))
+
+(defmethod tellStatus ((this aria2-controller) gid &optional keys)
+  "Return status of a download identified by GID."
+  (make-request "aria2.tellStatus" gid keys))
+
+(defmethod tellActive ((this aria2-controller) &optional keys)
+  "Return statuses of active downloads."
+  (make-request "aria2.tellActive" keys))
+
+(defmethod tellWaiting ((this aria2-controller) &optional offset num keys)
+  "Return statuses of waiting downloads."
+  (make-request "aria2.tellWaiting" (or offset 0) (or num most-positive-fixnum) keys))
+
+(defmethod tellStopped ((this aria2-controller) &optional offset num keys)
+  "Return statuses of stopped downloads."
+  (make-request "aria2.tellStopped" (or offset 0) (or num most-positive-fixnum) keys))
+
+(defmethod changePosition ((this aria2-controller) gid pos how)
+  "Change position of a download denoted by GID. POS is a number. HOW is one of:
+\"POS_SET\" - sets file to POS position from the beginning of a list (first element is 0),
+\"POS_CUR\" - moves file by POS places relative to it's current position,
+\"POS_END\" - sets file to POS position relative to end of list."
+  (unless (member how '("POS_SET" "POS_CUR" "POS_END"))
+    (signal 'aria2-err-no-such-position-type (list how)))
+  (make-request "aria2.changePosition" gid pos how))
+
+(defmethod changeUri ((test aria2-controller) gid file-index del-uris add-uris &optional position)
+  "This method removes the URIs in DEL-URIS list from and appends the URIs in ADD-URIS list
+to download denoted by GID. FILE-INDEX is 1-based position, identifying a file in a download.
+POSITION is a 0-based index specifying where URIs are inserted in waiting list.
+Returns a pair of numbers denoting amount of files deleted and files inserted."
+  (make-request "aria2.changeUri" gid file-index del-uris add-uris (or position 0)))
+
+(defmethod getOption ((this aria2-controller) gid)
+  "This method returns options of the download denoted by GID."
+  (make-request "aria2.getOption" gid))
+
+(defmethod changeOption ((this aria2-controller) gid options)
+  "Set OPTIONS for file denoted by GID. OPTIONS is an alist."
+  (make-request "aria2.changeOption" gid options))
+
+(defmethod getGlobalOption ((this aria2-controller))
+  "Return an alist of global options. Global options are used as defaults for newly added files."
+  (make-request "aria2.getGlobalOptions"))
+
+(defmethod changeGlobalOption ((this aria2-controller) options)
+  "Changes default global opts to OPTIONS. OPTIONS is an alist of opt-name and value."
+  (make-request "aria2.changeGlobalOption" options))
+
+(defmethod getGlobalStat ((this aria2-controller))
+  "Returns global statistics such as the overall download and upload speeds."
+  (make-request "aria2.getGlobalStat"))
+
+(defmethod purgeDownloadResult ((this aria2-controller))
+  "This method purges completed/error/removed downloads to free memory."
+  (make-request "aria2.purgeDownloadResult"))
+
+(defmethod removeDownloadResult ((this aria2-controller) gid)
+  "Removes a completed/error/removed download denoted by GID from memory."
+  (make-request "aria2.removeDownloadResult" gid))
+
+(defmethod saveSession ((this aria2-controller))
+  "Saves the current session to a `aria2-session-file' file."
+  (make-request "aria2.saveSession"))
+
+(defmethod shutdown ((this aria2-controller) &optional force)
+  "Shut down aria2c process. If FORCE don't wait for unregistering torrents."
+  (when (is-process-running this)
+    (make-request this (if force "aria2.forceShutdown" "aria2.shutdown"))
+    (oset this pid -1)))
+
+(defmethod getUris ((this aria2-controller) gid)
+  "Return a list of uris used in download identified by GID."
+  (make-request this "aria2.getUris" gid))
+
+(defmethod getFiles ((this aria2-controller) gid)
+  "Return a file list of a download identified by GID."
+  (make-request this "aria2.getFiles" gid))
+
+(defmethod getPeers ((this aria2-controller) gid)
+  "Returns a list peers of the download denoted by GID."
+  (make-request "aria2.getPeers" gid))
+
+(defmethod getServers ((this aria2-controller) gid)
+  "Returns currently connected HTTP(S)/FTP servers of the download denoted by GID."
+  (make-request "aria2.getServers" gid))
+
+;;; Major mode settings start here
+
+(defvar aria2--cc nil "Control center object container.")
+
+(defconst aria2--list-format (vector
+                              '("File" 30 t) '("Status" 7 t) '("Type" 7 t)
+                              '("Done" 4 t) '("Download" 8 t) '("Upload" 8 t))
+  "Format for downloads list columns.")
+
+(defun aria2--list-entries ()
+  "Returns entries to be displayed in downloads list."
+  (let* ((keys (vector "gid" "status" "totalLength" "completedLength" "downloadSpeed" "uploadSpeed"))
+         (active (tellActive aria2--cc))
+         (waiting (tellWaiting aria2--cc))
+         (stopped (tellStopped aria2--cc)))
+    ))
+
+(defvar aria2-mode-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map "j" 'next-line)
+    (define-key map "k" 'previous-line)
+    map)
+  "Keymap for `aria2-mode'.")
+
+(defcustom aria2-mode-hook nil
+  "Hook ran afer enabling `aria2-mode'."
+  :type 'hook
+  :group 'aria2)
+
+(define-derived-mode aria2-mode tabulated-list-mode "Aria2"
+  "Mode for controlling aria2 downloader.
+\\{aria2-mode-map}"
+  :group 'aria2
+  (unless (file-executable-p aria2-executable)
+    (signal 'aria2-err-no-executable nil))
+  ;; try to load controller state from file
+  (unless aria2--cc
+    (condition-case nil
+        (setq aria2-cc (eieio-persistent-read aria2--cc-file aria2-controller))
+      (error (setq aria2--cc (make-instance aria2-controller
+                                            "aria2-controller"
+                                            :file aria2--cc-file)))))
+  ;; list settings
+  (setq tabulated-list-format aria2--list-format)
+  (tabulated-list-init-header)
+  (setq tabulated-list-entries #'aria2--list-entries))
+
+;;;###autoload
+(defun aria2-downloads-list ()
+  "Display current aria2 downloads.  Enable `aria2-mode' to controll the process."
+  (interactive)
+  (with-current-buffer (pop-to-buffer aria2-list-buffer-name)
+    (aria2-mode)))
+
+(provide 'aria2-mode)
+
+;; Local Variables:
+;; coding: utf-8-unix
+;; indent-tabs-mode: nil
+;; End:
+
+;;; aria2-mode.el ends here
